67b1c437388ce5b2f1833d2f283b8d321c1e4f02
Adora <im.stariluz@gmail.com>
2025-04-06 05:18:38 -0600

    #38 Add transition manager and title screen controller

---
+++ Assets/Dev/stariluz/Scripts/TitleScreenController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TitleScreenController : MonoBehaviour
+{
+
+    [HideInInspector]
+    protected InputActions _inputActions;
+    public InputActions inputActions
+    {
+        get
+        {
+            return _inputActions;
+        }
+    }
+
+    [HideInInspector]
+    protected InputActions.UIActions _uiActions;
+    public InputActions.UIActions uiActions
+    {
+        get
+        {
+            return _uiActions;
+        }
+    }
+
+    [SerializeField] private string sceneToLoad = "Narrative1"; // Cambia esto por el nombre real de tu escena
+
+    
+    private void Awake()
+    {
+        _inputActions = new InputActions();
+        _uiActions = inputActions.UI;
+        _uiActions.FastNext.performed += StartGame;
+    }
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void StartGame(InputAction.CallbackContext context)
+    {
+        SceneTransitionManager.Instance.LoadScene(sceneToLoad);
+    }
+}
